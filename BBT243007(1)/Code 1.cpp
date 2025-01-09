477093428624b8aee3c3882058fc1a273f44c8bd
abdulrahm-10 <abdulrahimirfan06@gmail.com>
2025-01-09 20:41:16 +0500

    Product Code in C++

---
+++ BBT243007(1)/Code 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int A1,A2,Product;
+    cout<<"Enter Value of A1 = ";
+    cin>>A1;
+    cout<<"Enter Value of A2";
+    cin>>A2;
+    Product= A1*A2;
+    cout<<"Your Product is "<<Product;
+    return 0;
+}
